87627c466edb9359959c5f520d3971c3eda64f77
Дарья Давыдова <dddavydova_1@edu.hse.ru>
2020-07-23 13:14:48 +0300

    First commit message

---
+++ FirstProgram.py
@@ -0,0 +1,25 @@
+import json
+
+# строка которую будем парсить
+json_string = """ {
+  "orderID": 42,
+  "customerName": "John Smith",
+  "customerPhoneN": "555-1234",
+  "orderContents": [
+    {
+      "productID": 23,
+      "productName": "keyboard",
+      "quantity": 1
+    },
+    {
+      "productID": 13,
+      "productName": "mouse",
+      "quantity": 1
+    }
+  ],
+  "orderCompleted": true
+} """
+
+# распарсенная строка
+parsed_string = json.loads(json_string)
+print(parsed_string)
